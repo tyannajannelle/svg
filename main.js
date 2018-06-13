11a70f3e58cad5b7535fcb66862bd6ad7014a3ff
tyannajannelle <39134417+tyannajannelle@users.noreply.github.com>
2018-06-13 18:22:25 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  window.setTimeout(fadeout, 4000);
-}
-
-function fadeout() {
-  document.getElementById('svg-icon').style.opacity = '0';
-  document.getElementById('svg-icon2').style.opacity = '0';
-}
